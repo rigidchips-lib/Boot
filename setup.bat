6ece538e34d489b2c1398cde140085b8820ac603
lempiji <Koichi.Takio@gmail.com>
2014-08-09 17:29:16 +0900

    Add setup.bat
    管理者で実行する必要あり。
    -make lib dir
    -make link boot
    -add LUA_PATH

---
+++ setup.bat
@@ -0,0 +1,13 @@
+cd /d %~dp0
+
+SET TGT="%CD%\..\lib"
+SET ORGBOOT="%CD%\Boot"
+SET DSTBOOT="%CD%\..\lib\Boot"
+
+IF NOT EXIST %TGT% (MD %TGT%)
+
+IF NOT EXIST %DSTBOOT% (MKLINK %DSTBOOT% %ORGBOOT%)
+
+REG ADD hkcu\Environment /v LUA_PATH /t REG_SZ /d "%CD%\?" /f
+
+PAUSE
